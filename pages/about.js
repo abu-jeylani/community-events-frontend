2f2bab6a1e04e99df6977aeacf5e704d6ab67088
Abu Mohamed <48966850+moJeylani@users.noreply.github.com>
2022-11-29 13:16:30 -0500

    started next.js boilerplate of site

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+export default function about() {
+  return (
+    <Layout title="About Community Events">
+      <h1>About</h1>
+      <p>This is an app to find the latest events going on in our community </p>
+      <p> Version: 1.0.0</p>
+    </Layout>
+  );
+}
