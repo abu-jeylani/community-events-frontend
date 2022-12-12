22290945b7ce292f5f69a26edaa071f6796b3785
Abu Mohamed <48966850+moJeylani@users.noreply.github.com>
2022-12-11 19:04:50 -0700

    fixing padding again

--- components/Showcase.js
+++ components/Showcase.js
@@ -3,11 +3,11 @@ export default function Showcase() {
     <div className=" mt-8 h-72 w-full text-gray-200 flex justify-center items-center text-center px-62 relative drop-shadow-lg shadow-current  ">
       <div className="font-mono">
         <div className=" ">
-          <h1 className="animate-pulse text-4xl mb-2 ">
+          <h1 className="animate-pulse text-4xl mb-2 max-md:text-md ">
             Welcome <span className="animate-pulse">👋</span>
           </h1>
 
-          <h2 className=" text-lg">
+          <h2 className=" text-md max-md:text-sm max-md:px-4">
             Find the latest events going on in your community
           </h2>
         </div>
