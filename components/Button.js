5b1a6ed76c79fedb1c64ad60b4e1d26209663813
Abu Mohamed <48966850+moJeylani@users.noreply.github.com>
2022-12-11 20:59:48 -0700

    removing button

--- components/Button.js
+++ components/Button.js
@@ -3,13 +3,11 @@ import Link from "next/link";
 
 export default function Button({ title, path }) {
   return (
-    <button>
-      <Link
-        className="text-white shadow-sm shadow-slate-800 text-xs bg-slate-800 hover:bg-slate-700 px-6 py-3 rounded-2xl"
-        href={`${path}`}
-      >
-        {title}
-      </Link>
-    </button>
+    <Link
+      className="text-white shadow-sm shadow-slate-800 text-xs bg-slate-800 hover:bg-slate-700 px-6 py-3 rounded-2xl"
+      href={`${path}`}
+    >
+      {title}
+    </Link>
   );
 }
